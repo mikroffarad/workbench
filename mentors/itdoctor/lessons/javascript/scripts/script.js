3152cc7b0a46514a91d8f7d38a217f11a1964cdc
Victor Zaritsky <42099665+mikroffarad@users.noreply.github.com>
2023-05-28 00:17:48 +0300

    Task 4

--- mentors/itdoctor/lessons/javascript/scripts/script.js
+++ mentors/itdoctor/lessons/javascript/scripts/script.js
@@ -1,6 +1,15 @@
-let arr = []
+let arr = [47, 12, 85, 63, 29, 54, 91, 38, 77, 16, 42, 59, 88, 25, 73, 32, 66, 95, 19, 81, 92];
 
-for (let i = 0; i < 10; i++) {
-    arr.push(i**2);
-}
-console.log(arr)
+// for (i of arr) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+let arrNew = arr.filter(function (item) {
+    if (item % 2 === 0) {
+        return item
+    }
+})
+
+console.log(arrNew.length)
