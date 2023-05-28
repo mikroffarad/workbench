08677f79496b35d300e9d0333d8e2c16291c3db0
Victor Zaritsky <42099665+mikroffarad@users.noreply.github.com>
2023-05-28 22:12:53 +0300

    Task 1-4

--- mentors/itdoctor/lessons/javascript/scripts/script.js
+++ mentors/itdoctor/lessons/javascript/scripts/script.js
@@ -1,15 +1,45 @@
-let arr = [47, 12, 85, 63, 29, 54, 91, 38, 77, 16, 42, 59, 88, 25, 73, 32, 66, 95, 19, 81, 92];
-
-// for (i of arr) {
-//     if (i % 2 === 0) {
-//         console.log(i)
+// let human = {
+//     name: "Anton",
+//     age: 19,
+//     introduceMyself() {
+//         return `Hello, my name is ${this.name}, I'm ${this.age} years old`;
 //     }
 // }
+// console.log(human.introduceMyself())
 
-let arrNew = arr.filter(function (item) {
-    if (item % 2 === 0) {
-        return item
-    }
-})
+// let arr = [99,88,77,66,555,44,33,22,1111];
+// let arrSorted = arr.sort((a,b) => a-b) 
+// console.log(arrSorted)
 
-console.log(arrNew.length)
+// function fibonacci(count) {
+//     let fib = [];
+//     for (let i = 0; i < count; i++) {
+//         if (i === 0) {
+//             fib[i] = 1;
+//         } else if (i === 1) {
+//             fib[i] = 2;
+//         } else {
+//             fib[i] = fib[i-2] + fib[i-1]; 
+//         }
+//     }
+//     return fib;
+// }
+// console.log(fibonacci(64))
+
+// function factorial(n) {
+//     let value = 1;
+//     if (n === 0 || n === 1) {
+//         return 1;
+//     }
+//     for(let i = 1; i <= n; i++) {
+//         value *= i;
+//     }
+//     return value;
+// }
+// console.log(factorial(4))
+
+// function sumNum(n) {
+//     return n.toString().split('').map((i) => +i).forEach((value, index, array) => array[index] = 5);
+// }
+//
+// console.log(sumNum(65536))
