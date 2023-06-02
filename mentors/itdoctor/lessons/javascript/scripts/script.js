edc4aa87c5ab862e09d42920ab055e6a525293ab
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2023-06-02 11:45:38 +0300

    guessTheNumber

--- mentors/itdoctor/lessons/javascript/scripts/script.js
+++ mentors/itdoctor/lessons/javascript/scripts/script.js
@@ -1,70 +1,16 @@
-// let human = {
-//     name: "Anton",
-//     age: 19,
-//     introduceMyself() {
-//         return `Hello, my name is ${this.name}, I'm ${this.age} years old`;
-//     }
-// }
-// console.log(human.introduceMyself())
-
-// let arr = [99,88,77,66,555,44,33,22,1111];
-// let arrSorted = arr.sort((a,b) => a-b)
-// console.log(arrSorted)
-
-// function fibonacci(count) {
-//     let fib = [];
-//     for (let i = 0; i < count; i++) {
-//         if (i === 0) {
-//             fib[i] = 1;
-//         } else if (i === 1) {
-//             fib[i] = 2;
-//         } else {
-//             fib[i] = fib[i-2] + fib[i-1];
-//         }
-//     }
-//     return fib;
-// }
-// console.log(fibonacci(64))
-
-// function factorial(n) {
-//     let value = 1;
-//     if (n === 0 || n === 1) {
-//         return 1;
-//     }
-//     for(let i = 1; i <= n; i++) {
-//         value *= i;
-//     }
-//     return value;
-// }
-// console.log(factorial(4))
-
-// function sumNum(n) {
-//     return n.toString().split('').map((i) => +i).forEach((value, index, array) => array[index] = 5);
-// }
-//
-// console.log(sumNum(65536))
-
-// function addMul(n) {
-//   let arr = n.toString().split('').map((i) => +i)
-//   let sum = 0, mul = 1;
-//   for (i of arr) {
-//     sum += i;
-//     mul *= i;
-//   }
-//   return {
-//     "Sum": sum,
-//     "Mul": mul
-//   };
-// }
-// console.log(addMul(795));
-
-function numReverse(num) {
-  let arr = num.toString().split('');
-  let newArr = []
-
-  for (i = arr.length-1; i >= 0; i--) {
-    newArr.push(arr[i])
+function guessTheNumber(num) {
+  const guessed = Math.floor(Math.random() * 100);
+  
+  for (let attempts = 10; attempts >= 0; attempts--) {
+    let variant = prompt(`Attempt №${attempts} Your Number`);
+    let moreOrLess = variant > guessed ? "lower" : "more"
+    if (variant != guessed) {
+      console.log(`Try ${moreOrLess}. Attempts left: ${attempts}`)
+    } else {
+      return `You win. Random Number: ${guessed}. You had ${10-attempts} attempts to guess that`;
+    }
   }
-  return +newArr.join('');
+  console.log("You lose")
 }
-console.log(numReverse(8192));
+
+console.log(guessTheNumber());
