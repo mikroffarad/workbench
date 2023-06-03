9cee8b2b0de6d5d47947beb77f02685a520c771f
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2023-06-03 18:33:12 +0300

    js date managment

--- mentors/itdoctor/lessons/javascript/scripts/script.js
+++ mentors/itdoctor/lessons/javascript/scripts/script.js
@@ -1,20 +1,13 @@
-function guessTheNumber(num) {
-  const guessed = Math.floor(Math.random() * 100);
-  
-  for (let attempts = 1; attempts <= 10; attempts++) {
-    let variant = prompt(`Attempt №${attempts} Your Number`);
-    let moreOrLess = variant > guessed ? "lower" : "more"
-    switch (true) {
-      case variant != guessed && attempts === 10:
-        return alert(`You lose. Guessed number: ${guessed}`);
-        break;
-      case variant != guessed:
-        alert(`${guessed} Try ${moreOrLess}. Attempts left: ${10 - attempts}`);
-        break;
-      default:
-        return alert(`You win. Random Number: ${guessed}. You had ${attempts} attempts to guess that`);
-        break;
-    }
-  }
+let a = Date.now(new Date());
+
+// console.log(Date.parse(a));
+// console.log(a.getMilliseconds());
+// a.setFullYear(a.getFullYear() + 4);
+// console.log(a);
+
+for (let i = 0; i < 100; i++) {
+  console.log(0);
 }
-guessTheNumber()
+
+let b = Date.now(new Date());
+console.log(b-a);
