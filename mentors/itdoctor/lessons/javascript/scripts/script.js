6c292f59a8f2b414dc37a195c91b15c2f9ee6d87
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2023-06-04 18:45:48 +0300

    timeToMinutes() function JSDoc

--- mentors/itdoctor/lessons/javascript/scripts/script.js
+++ mentors/itdoctor/lessons/javascript/scripts/script.js
@@ -1,16 +1,14 @@
-let a = new Date(2004, 09, 05, 10, 00, 00);
-console.log(a);
-console.log(a.toDateString());
-let options = {
-  era: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-  weekday: "long",
-  timezone: "UTC",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
+/**
+ * Function converts time to minutes
+ * @param {string} time accept values like "11:34"
+ * 
+ * @returns {number} if function accepts "02:08", function returns "128" 
+ */
+
+function timeToMinutes(time) {
+  const hours = +time.split(':')[0];
+  const minutes = +time.split(':')[1];
+  return hours * 60 + minutes;
 }
 
-document.write(`I was born in ${a.toLocaleString("en", options)}`);
+console.log(timeToMinutes("02:08"));
