baf9ff4415077ee7b2641d683d7fcb2665c40eba
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2023-07-21 16:39:34 +0300

    added deals section

--- mentors/freelancerls/layouts/realestate/scripts/script.js
+++ mentors/freelancerls/layouts/realestate/scripts/script.js
@@ -8,3 +8,29 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.body-main-block__arrow.swiper-button-prev',
   },
 });
+
+// Switch between tabs
+
+const tabNavigationItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.tabs-deals__tab');
+
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let targetActiveIndex = null;
+  if (targetElement.closest('.tabs-deals__button')) {
+    tabNavigationItems.forEach((tabNavigationItem, index) => {
+      if (tabNavigationItem.classList.contains('active')) {
+        currentActiveIndex = index;
+        tabNavigationItem.classList.remove('active');
+      }
+      if (tabNavigationItem === targetElement) {
+        targetActiveIndex = index;
+      }
+    });
+    targetElement.classList.add('active');
+    tabItems[targetActiveIndex].classList.add('active');
+    tabItems[currentActiveIndex].classList.remove('active')
+  }
+});
+
