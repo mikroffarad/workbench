7447fc99af7670f2b19fc7f7b21897efc13422fb
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2023-09-15 17:20:28 +0300

    type section

--- own/layouts/findYourJob/js/script.js
+++ own/layouts/findYourJob/js/script.js
@@ -9,3 +9,35 @@ function documentClick(e) {
 		document.documentElement.classList.toggle('menu-open')
 	}
 }
+
+// Button toggle switcher
+document.addEventListener('DOMContentLoaded', function () {
+	const buttons = document.querySelectorAll('.item-navigation');
+
+	buttons.forEach(function (button) {
+		button.addEventListener('click', function () {
+			// Remove the 'active' class from all buttons
+			buttons.forEach(function (btn) {
+				btn.classList.remove('active');
+			});
+
+			// Add the 'active' class to the clicked button
+			this.classList.add('active');
+		});
+	});
+});
+
+// Categories list
+const categoriesList = [
+	{
+		"icon": "design",
+		"title": "Design"
+	},
+	{
+		"icon": "develop",
+		"title": "Development"
+	},
+]
+
+const buttonNavigationNode = document.createElement("button");
+buttonNavigationNode.classList.add("")
