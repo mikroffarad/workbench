8c0898df19c0e56e6a941e31b2f7fa04e3ca49eb
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2023-09-27 18:36:20 +0300

    some chages in project list

---
+++ own/snippets/project-list/2023/scripts/data.js
@@ -0,0 +1,140 @@
+const projectsList = {
+    "Single Pages": {
+        "icon": `<i class="fa-solid fa-file"></i>`,
+        "subTabs": {
+            "Made by Tutorials": {
+                "icon": `<i class="fa-brands fa-youtube"></i>`,
+                "projects": [
+                    {
+                        "title": "проєкт1",
+                        "customStyles": "background: #0f0; color: #000;",
+                        "icon": "",
+                        "link": ""
+                    },
+                    {
+                        "name": "проєкт2"
+                    },
+                    {
+                        "name": "проєкт3"
+                    },
+                    {
+                        "name": "проєкт4"
+                    },
+                    {
+                        "name": "проєкт5"
+                    },
+                ]
+            },
+            "Made my own": {
+                "icon": `<i class="fa-solid fa-fingerprint"></i>`,
+                "projects": [
+                    {
+                        "name": "проєкт6"
+                    },
+                    {
+                        "name": "проєкт7"
+                    },
+                    {
+                        "name": "проєкт8"
+                    },
+                    {
+                        "name": "проєкт9"
+                    },
+                    {
+                        "name": "проєкт10"
+                    },
+                ]
+            }
+        }
+    },
+    "Multi Pages": {
+        "icon": `<i class="fa-solid fa-sitemap"></i>`,
+        "subTabs": {
+            "Made by Tutorials": {
+                "icon": `<i class="fa-brands fa-youtube"></i>`,
+                "projects": [
+                    {
+                        "name": "проєкт11"
+                    },
+                    {
+                        "name": "проєкт12"
+                    },
+                    {
+                        "name": "проєкт13"
+                    },
+                    {
+                        "name": "проєкт14"
+                    },
+                    {
+                        "name": "проєкт15"
+                    },
+                ]
+            },
+            "Made my own": {
+                "icon": `<i class="fa-solid fa-fingerprint"></i>`,
+                "projects": [
+                    {
+                        "name": "проєкт16"
+                    },
+                    {
+                        "name": "проєкт17"
+                    },
+                    {
+                        "name": "проєкт18"
+                    },
+                    {
+                        "name": "проєкт19"
+                    },
+                    {
+                        "name": "проєкт20"
+                    },
+                ]
+            }
+        }
+    },
+    "Web Apps": {
+        "icon": `<i class="fa-solid fa-window-restore"></i>`,
+        "subTabs": {
+            "Made by Tutorials": {
+                "icon": `<i class="fa-brands fa-youtube"></i>`,
+                "projects": [
+                    {
+                        "name": "проєкт21"
+                    },
+                    {
+                        "name": "проєкт22"
+                    },
+                    {
+                        "name": "проєкт23"
+                    },
+                    {
+                        "name": "проєкт24"
+                    },
+                    {
+                        "name": "проєкт25"
+                    },
+                ]
+            },
+            "Made my own": {
+                "icon": `<i class="fa-solid fa-fingerprint"></i>`,
+                "projects": [
+                    {
+                        "name": "проєкт26"
+                    },
+                    {
+                        "name": "проєкт27"
+                    },
+                    {
+                        "name": "проєкт28"
+                    },
+                    {
+                        "name": "проєкт29"
+                    },
+                    {
+                        "name": "проєкт30"
+                    },
+                ]
+            }
+        }
+    },
+}
--- own/snippets/project-list/2023/scripts/tabs.js
+++ own/snippets/project-list/2023/scripts/tabs.js
@@ -1,141 +1,3 @@
-const projectsList = {
-    "Single Pages": {
-        "icon": `<i class="fa-solid fa-file"></i>`,
-        "subTabs": {
-            "Made by Tutorials": {
-                "icon": `<i class="fa-brands fa-youtube"></i>`,
-                "projects": [
-                    {
-                        "name": "проєкт1"
-                    },
-                    {
-                        "name": "проєкт2"
-                    },
-                    {
-                        "name": "проєкт3"
-                    },
-                    {
-                        "name": "проєкт4"
-                    },
-                    {
-                        "name": "проєкт5"
-                    },
-                ]
-            },
-            "Made my own": {
-                "icon": `<i class="fa-solid fa-fingerprint"></i>`,
-                "projects": [
-                    {
-                        "name": "проєкт6"
-                    },
-                    {
-                        "name": "проєкт7"
-                    },
-                    {
-                        "name": "проєкт8"
-                    },
-                    {
-                        "name": "проєкт9"
-                    },
-                    {
-                        "name": "проєкт10"
-                    },
-                ]
-            }
-        }
-    },
-    "Multi Pages": {
-        "icon": `<i class="fa-solid fa-sitemap"></i>`,
-        "subTabs": {
-            "Made by Tutorials": {
-                "icon": `<i class="fa-brands fa-youtube"></i>`,
-                "projects": [
-                    {
-                        "name": "проєкт11"
-                    },
-                    {
-                        "name": "проєкт12"
-                    },
-                    {
-                        "name": "проєкт13"
-                    },
-                    {
-                        "name": "проєкт14"
-                    },
-                    {
-                        "name": "проєкт15"
-                    },
-                ]
-            },
-            "Made my own": {
-                "icon": `<i class="fa-solid fa-fingerprint"></i>`,
-                "projects": [
-                    {
-                        "name": "проєкт16"
-                    },
-                    {
-                        "name": "проєкт17"
-                    },
-                    {
-                        "name": "проєкт18"
-                    },
-                    {
-                        "name": "проєкт19"
-                    },
-                    {
-                        "name": "проєкт20"
-                    },
-                ]
-            }
-        }
-    },
-    "Web Apps": {
-        "icon": `<i class="fa-solid fa-window-restore"></i>`,
-        "subTabs": {
-            "Made by Tutorials": {
-                "icon": `<i class="fa-brands fa-youtube"></i>`,
-                "projects": [
-                    {
-                        "name": "проєкт21"
-                    },
-                    {
-                        "name": "проєкт22"
-                    },
-                    {
-                        "name": "проєкт23"
-                    },
-                    {
-                        "name": "проєкт24"
-                    },
-                    {
-                        "name": "проєкт25"
-                    },
-                ]
-            },
-            "Made my own": {
-                "icon": `<i class="fa-solid fa-fingerprint"></i>`,
-                "projects": [
-                    {
-                        "name": "проєкт26"
-                    },
-                    {
-                        "name": "проєкт27"
-                    },
-                    {
-                        "name": "проєкт28"
-                    },
-                    {
-                        "name": "проєкт29"
-                    },
-                    {
-                        "name": "проєкт30"
-                    },
-                ]
-            }
-        }
-    },
-}
-
 const mainTabNavigation = document.querySelector(".main-tabs__navigation");
 const subTabNavigation = document.querySelector(".sub-tabs__navigation");
 const projectsListElement = document.querySelector(".projects__list");
@@ -203,8 +65,23 @@ function outputProjects(projects) {
     projectsListElement.innerHTML = "";
 
     for (const project of projects) {
-        const projectItem = document.createElement("li");
-        projectItem.textContent = project.name;
+        const projectItem = document.createElement("a");
+        projectItem.href = project.link;
+        projectItem.classList.add("projects__item");
+        projectItem.style = project.customStyles;
+
+        const projectIcon = document.createElement("img");
+        projectIcon.src = project.icon;
+        projectIcon.alt = "icon";
+        projectIcon.classList.add("projects__icon");
+
+        const projectTitle = document.createElement("h2");
+        projectTitle.textContent = project.title;
+        projectTitle.classList.add("projects__title");
+
+        projectItem.appendChild(projectIcon);
+        projectItem.appendChild(projectTitle);
+
         projectsListElement.appendChild(projectItem);
     }
 }
