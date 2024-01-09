88f480c9947c86ed16905cfe9db55c7da799b9cb
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2024-01-09 10:49:27 +0200

    added magicMouse

---
+++ tutorialProjects/snippets/hyperplexed/magicMouse/js/script.js
@@ -0,0 +1,56 @@
+let prevX = null;
+let prevY = null;
+let isLeftMouseButtonPressed = false;
+
+const wrapperNode = document.querySelector(".wrapper");
+
+wrapperNode.addEventListener("mousemove", (evt) => {
+    const currentX = evt.clientX;
+    const currentY = evt.clientY;
+
+    if ((prevX !== null && prevY !== null) && isLeftMouseButtonPressed === false) {
+        if (getDistance(prevX, prevY, currentX, currentY) >= 20) {
+            createStar()
+            console.log(isLeftMouseButtonPressed);
+        }
+    } else {
+        if (isLeftMouseButtonPressed !== true) {
+            createStar()
+        }
+    }
+
+    function createStar() {
+        const dot = document.createElement("div");
+        dot.className = "dot fa-solid fa-star";
+
+        dot.style.left = `${currentX}px`;
+        dot.style.top = `${currentY}px`;
+        wrapperNode.appendChild(dot);
+
+        prevX = currentX;
+        prevY = currentY;
+    }
+})
+
+wrapperNode.addEventListener("mousedown", (evt) => {
+    if (evt.button === 0) {
+        isLeftMouseButtonPressed = true
+        console.log("pressed");
+    }
+})
+
+wrapperNode.addEventListener("mouseup", (evt) => {
+    if (evt.button === 0) {
+        isLeftMouseButtonPressed = false;
+        console.log("released");
+    }
+})
+
+wrapperNode.addEventListener("contextmenu", (evt) => {
+    evt.preventDefault();
+    wrapperNode.innerHTML = ""
+})
+
+function getDistance(prevX, prevY, currentX, currentY) {
+    return Math.sqrt(Math.pow(currentX - prevX, 2) + Math.pow(currentY - prevY, 2));
+}
