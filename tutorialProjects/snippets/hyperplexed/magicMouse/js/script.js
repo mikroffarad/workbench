d6dc2e5d2326b9cb2d2ad358322fc3782a75f911
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2024-01-09 14:04:30 +0200

    finished magicMouse

--- tutorialProjects/snippets/hyperplexed/magicMouse/js/script.js
+++ tutorialProjects/snippets/hyperplexed/magicMouse/js/script.js
@@ -3,15 +3,17 @@ let prevY = null;
 let isLeftMouseButtonPressed = false;
 
 const wrapperNode = document.querySelector(".wrapper");
+const colorsArr = ["#2563eb", "#a855f7", "#e11d48", "#16a34a"]
+const animationsArr = ["fall-1", "fall-2", "fall-3"]
+const countNode = document.getElementById("count")
 
 wrapperNode.addEventListener("mousemove", (evt) => {
     const currentX = evt.clientX;
     const currentY = evt.clientY;
 
     if ((prevX !== null && prevY !== null) && isLeftMouseButtonPressed === false) {
-        if (getDistance(prevX, prevY, currentX, currentY) >= 20) {
-            createStar()
-            console.log(isLeftMouseButtonPressed);
+        if (getDistance(prevX, prevY, currentX, currentY) >= 50) {
+            createStar();
         }
     } else {
         if (isLeftMouseButtonPressed !== true) {
@@ -19,20 +21,57 @@ wrapperNode.addEventListener("mousemove", (evt) => {
         }
     }
 
-    function createStar() {
-        const dot = document.createElement("div");
-        dot.className = "dot fa-solid fa-star";
+    createGlow(prevX, prevY, currentX, currentY);
+    countNode.innerHTML = document.querySelectorAll(".star").length;
 
-        dot.style.left = `${currentX}px`;
-        dot.style.top = `${currentY}px`;
-        wrapperNode.appendChild(dot);
+    function createStar() {
+        const star = document.createElement("div");
+        star.className = "star fa-solid fa-star";
+
+        star.style.left = `${currentX}px`;
+        star.style.top = `${currentY}px`;
+        star.style.color = getRandom(colorsArr);
+        star.style.animationName = getRandom(animationsArr);
+
+        wrapperNode.appendChild(star);
 
         setTimeout(() => {
-            wrapperNode.removeChild(dot)
+            wrapperNode.removeChild(star)
         }, 1500)
 
         prevX = currentX;
         prevY = currentY;
+    }
+
+    function createGlow(prevX, prevY, currentX, currentY) {
+        const distance = getDistance(prevX, prevY, currentX, currentY),
+            quantity = Math.max(
+                Math.floor(distance / 100),
+                1
+            );
+        const dx = (currentX - prevY) / quantity,
+            dy = (currentY - prevY) / quantity;
+
+        Array.from(Array(quantity)).forEach((_, index) => {
+            const x = prevX + dx * index,
+                y = prevY + dy * index;
+
+            createGlowPoint(x, y);
+        });
+    }
+
+    function createGlowPoint(posX, posY) {
+        const glow = document.createElement("div");
+
+        glow.className = "dot";
+
+        glow.style.left = `${posX}px`;
+        glow.style.top = `${posY}px`;
+
+        wrapperNode.appendChild(glow)
+        setTimeout(() => {
+            wrapperNode.removeChild(glow)
+        }, 400)
     }
 })
 
@@ -57,4 +96,8 @@ wrapperNode.addEventListener("contextmenu", (evt) => {
 
 function getDistance(prevX, prevY, currentX, currentY) {
     return Math.sqrt(Math.pow(currentX - prevX, 2) + Math.pow(currentY - prevY, 2));
+}
+
+function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
