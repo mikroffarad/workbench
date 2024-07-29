e2c0e13796f0b2cf7da8f33da0c0fe9680a24936
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2024-07-29 23:26:27 +0300

    added localStorage feature

--- tutorialProjects/webApps/webcademy/GoogleKeep-JS/js/script.js
+++ tutorialProjects/webApps/webcademy/GoogleKeep-JS/js/script.js
@@ -4,40 +4,56 @@ const noteTitleInput = document.querySelector("#noteTitleInput");
 const noteTextInput = document.querySelector("#noteTextInput");
 const cardWrapper = document.querySelector("#cardWrapper")
 
+let notesArray = []
+
+// Handling localStorage
+if (localStorage.getItem("notesArray")) {
+    notesArray = JSON.parse(localStorage.getItem("notesArray"))
+}
+
 // Array with notes
-const notesArray = [
-    {
-        id: 1,
-        title: 'JS Basics',
-        text: 'Lorem ipsum dolor sit amet...'
-    },
-    {
-        id: 2,
-        title: 'DOM Tree',
-        text: 'Lorem ipsum dolor sit amet...'
-    },
-    {
-        id: 3,
-        title: 'JS Events',
-        text: 'Lorem ipsum dolor sit amet...'
-    },
-]
+// const notesArray = [
+//     {
+//         id: 1,
+//         title: 'JS Basics',
+//         text: 'Lorem ipsum dolor sit amet...'
+//     },
+//     {
+//         id: 2,
+//         title: 'DOM Tree',
+//         text: 'Lorem ipsum dolor sit amet...'
+//     },
+//     {
+//         id: 3,
+//         title: 'JS Events',
+//         text: 'Lorem ipsum dolor sit amet...'
+//     },
+// ]
 
 // Listen submit of form
 addNoteForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
+
+    let id = 1;
+
+    if (notesArray.length > 0) {
+        id = notesArray[notesArray.length - 1]["id"] + 1;
+    }
+
     // Add text of note into array
     notesArray.push({
-        id: notesArray[notesArray.length - 1]["id"] + 1,
+        id: id,
         title: noteTitleInput.value,
-        text: noteTextInput.textContent
+        text: noteTextInput.innerHTML
     })
+
+    localStorage.setItem("notesArray", JSON.stringify(notesArray));
 
     // Clear input value by clearing the entire form
     addNoteForm.reset();
-    noteTitleInput.textContent = "";
-    noteTextInput.textContent = "";
+    noteTitleInput.innerHTML = "";
+    noteTextInput.innerHTML = "";
     formTextFake.removeAttribute("hidden");
     noteTitleInput.focus();
 
@@ -79,7 +95,7 @@ document.addEventListener("click", (event) => {
             return note.id == id
         })
         notesArray.splice(index, 1)
-
+        localStorage.setItem("notesArray", JSON.stringify(notesArray));
         event.target.closest(".card").remove();
     }
 
